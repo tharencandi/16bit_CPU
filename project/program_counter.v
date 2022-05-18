bb4185da731555cd9ae2c06215de61100877648a
Remon Muhana <rmuh5776@uni.sydney.edu.au>
2022-05-18 16:06:23 +1000

    Update program_counter.v

--- project/program_counter.v
+++ project/program_counter.v
@@ -11,14 +11,7 @@ module program_counter(clk, rst, p_in, bus, select, pc_enable, out);
 	if (select <= 1)
 		mux_out <= bus
 	end
-		
-	always@(posedge clk or posedge rst) begin
-		if (rst == 1'b1)
-			out <= 16'b0
-		else if(pc_enable == 1'b1)
-			out <= mux_out
-	end
-			
+		sixteen_bit_reg pc_reg(.D(mux_out) .clk(clk) .rst(rst) .Q(out) .enable(pc_enable)) 
 endmodule
 			
 			
